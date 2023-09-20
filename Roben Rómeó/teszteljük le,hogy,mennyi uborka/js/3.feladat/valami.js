35c654d3cabc3e53ba0c326e53cbadc1b4596b12
Roben Romeo <romeo.roben@katonaj-mkszig.hu>
2023-09-20 10:05:30 +0200

    yayayayayayayayayayayayayaayayayyayayyayayayayayyayayayayyayayayayyayayayayyayayayayayayayayyayayayayyayayayayay

---
+++ Roben Rómeó/teszteljük le,hogy,mennyi uborka/js/3.feladat/valami.js
@@ -0,0 +1,28 @@
+/*var keresztnev = "Peti";
+console.log(keresztnev);
+var vezeteknev = "Pedo"
+console.log(vezeteknev);
+
+var kor = 69;
+console.log(kor);
+
+var nagykoru = false;
+console.log(nagykoru);
+
+var magassag = 160;
+console.log(magassag);
+
+//var function;
+//console.log(magassag);*/
+
+var keresztnev = "Peti";
+var kor = 69;
+
+console.log(keresztnev + ' ' + kor);
+var vezeteknev, felnott;
+vezeteknev = "Pedo";
+//felnott = true
+
+console.log(vezeteknev + ' felnőtt? ' + felnott);
+kor = 'harminc'
+
