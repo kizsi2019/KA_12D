89cdb1cef1cb05ffc4cfd676aa1dc18538dcd939
Rozsahegyi Akos <akos.rozsahegyi@katonaj-mkszig.hu>
2023-09-21 13:25:40 +0200

    Javascript 2023-09-21

---
+++ Rózsahegyi Ákos/JS/kilencedik_resz/script.js
@@ -0,0 +1,21 @@
+// A Boolean logika
+
+/*
+var sutANap = true;
+var joIdoVan = false;
+
+if (sutANap && joIdoVan) {
+    console.log("Jó kedvem van")
+} else {
+    console.log("Rossz kedvem van")
+}
+*/
+
+var sutANap = true;
+var joIdoVan = false;
+
+if (sutANap || joIdoVan) {
+    console.log("Jó kedvem van")
+} else {
+    console.log("Rossz kedvem van")
+}
