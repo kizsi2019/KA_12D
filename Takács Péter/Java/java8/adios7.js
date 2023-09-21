7d92d6aae5b1e68773031a1f1a1d0c82485dc47b
unknown <peter.takacs@katonaj-mkszig.hu>
2023-09-21 13:31:11 +0200

    asd

---
+++ Takács Péter/Java/java8/adios7.js
@@ -0,0 +1,139 @@
+/*var keresztNev = 'Pisti';
+var a = 2;
+console.log(keresztNev);
+var vezetekNev= 'Kis';
+console.log(vezetekNev);
+
+var kor = 32;
+console.log(kor)
+
+var nagykoru = false;
+console.log(nagykoru);
+
+var magassag = 171;
+console.log(magassag);
+console.log(nemLetezo);
+//ismétlő gyakorlatok
+
+var keresztNev = 'Pisti';
+var a = 2;
+
+console.log(keresztNev + ' ' + kor);
+
+var vezetekNev, felnott;
+vezetekNev = 'Kovács';
+//felnott = true;ű
+
+console.log(vezetekNev + 'felnött' + felnott);
+
+kor = 'harminc';*/
+
+/*matematikai operátorok
+var szam = 10;
+var osszeg = szam + 33;
+console.log(osszeg);
+var kulonbseg = 100 - szam;
+console.log(kulonbseg);
+
+console.log(szam * 2);
+console.log(szam/3);
+
+//logikaiop.
+var szam1 = 100;
+var szam2 = 100;
+var egyenlo = szam1 = szam2;
+console.log(egyenlo);
+
+//typeof
+console.log(typeof szam1)
+
+var fiuk = 23;
+var lanyok = 35;
+
+if (fiuk > lanyok) {
+    console.log('a diuk vannak többen');
+}
+else {
+    console.log('a lányok többen vannak');
+}
+
+// ---
+
+var belaKora = 12;
+var felnott = false;
+if(felnott) {
+    console.log('Béla már felnőt');
+}
+else 
+{
+    console.log('Béci még gyerek');
+}
+
+var film = "Gladiator";
+var mufaj;
+
+switch (film){
+    case "shrek" : mufaj = "mese";
+        break;
+    case "terminator" : mufaj = "akcio";
+        break;
+    case "utazók" : mufaj = "sci-fi";
+        break;
+    default: 
+        mufaj = " besorolatlan"
+} 
+console.log(mufaj);
+
+var kor = 40;
+var nev = "Béla ";
+
+switch(true){
+    case kor < 13 :
+        console.log(nev + "kisfiu");
+        break;
+    case kor >= 13 && kor <= 20:
+        console.log(nev + "tinédzser");
+        break;
+    case kor >= 20 && kor < 30:
+        console.log(nev + "fiatal ember");
+        break;
+    default:
+        console.log(nev + " igazi férfi");
+
+
+}
+
+var kigyok = 100;
+var békák = 78;
+var madarak = 198;
+
+var tobbAHullo = kigyok + békák > madarak;
+console.log(tobbAHullo);
+
+var atlag = (kigyok+madarak+békák)/3;
+
+console.log(atlag);
+
+var a,b;
+console.log(a);
+a = b = (10+1)*2-2;
+
+console.log(a,b);
+
+a *= 2;
+console.log(a);
+
+a = a + 1;
+a += 1;
+a++;
+*/
+
+var sutANap = false;
+var joIDoVan = true;
+
+if (sutANap != false || joIDoVan != false){
+    console.log("Jó kedvem van!");
+}
+else {
+    console.log("rossz kedvem van!")
+}
---
+++ Takács Péter/c#/ismétlés1/ismétlés1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ismétlés1
+{
+    namespace PeldaOperatorok
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Beep(1000, 100000);
+                //17 lesz az eredmény
+                var kifejezes = 3 * 6 - 2 + 1 % 2;
+                Console.WriteLine(kifejezes);
+
+                //így már helyes és az eredmény 1
+                var kifejezes2 = (3 * 6 - 2 + 1) % 2;
+                Console.WriteLine(kifejezes2);
+
+                Console.Write("A long tipus merete byte-ban: ");
+                //8
+                int bytes = sizeof(long);
+                Console.WriteLine(bytes);
+
+                //binárisan
+                // 0000_0001 << 6 => 0100_0000
+                int kettohat = 1 << 6;
+                Console.WriteLine(kettohat);
+
+                //binárisan
+                //1111_0000 >> 2 => 0011_1100
+                int balra = 240 >> 2;
+                Console.WriteLine(balra);
+
+                //true
+                bool logika = 33 > 22;
+                //false
+                bool logika2 = (33 / 2) == 0;
+                Console.WriteLine(logika);
+                Console.WriteLine(logika2);
+
+                string szoveg = "ez egy";
+                szoveg += " szép mondat.";
+
+                Console.WriteLine(szoveg);
+
+                int x = 3;
+                //4 lesz, mert inkrementálás után ír ki
+                Console.WriteLine(++x);
+                x = 3;
+                //3 lesz, mert kiír és csak utána inkrementálja a változót
+                Console.WriteLine(x++);
+                //4 lesz, mert itt már a növelt értéket látjuk
+                Console.WriteLine(x);
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
