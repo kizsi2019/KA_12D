8a35f5ec862f861d736a9bdaf8d958fb393896c3
Balint420 <77265532+Balint420@users.noreply.github.com>
2023-09-28 09:38:40 +0200

    Add files via upload

---
+++ Hajdu Bálint János/c#/Program.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Globalization;
+
+namespace PeldaStringFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int ma = 21;
+            int tegnap = 18;
+            var szoveg = string.Format("Ma {0} fok volt, tegnap pedig {1}", ma, tegnap);
+
+            Console.WriteLine(szoveg);
+
+            Console.WriteLine("Így is lehet: {0}, {1}", ma, tegnap);
+            Console.ReadKey();
+            
+            Console.WriteLine("Pénz: {0:C1}", 3148);
+            Console.WriteLine("Százalék: {0:P3}", 0.123456);
+            Console.WriteLine("Általános: {0:G}", 132354);
+            Console.WriteLine("Exponenciális: {0:E}", 132354);
+            Console.WriteLine("Hexa: {0:X}", 255);
+            Console.ReadKey();
+            
+
+            double d = 1500.42;
+
+            //1500,4200
+            Console.WriteLine("{0:00.0000}", d);
+            //(1500),42
+            Console.WriteLine("{0:(#).##}", d);
+            //1500,4
+            Console.WriteLine("{0:0.0}", d);
+            //1 500
+            Console.WriteLine("{0:0,0}", d);
+            //150042%
+            Console.WriteLine("{0:0%}", d);
+            //[Foo  ]
+            Console.WriteLine("[{0,-5}]", "Foo");
+            //[  Foo]
+            Console.WriteLine("[{0,5}]", "Foo");
+
+            //pozitív
+            Console.WriteLine("{0:pozitív;negatív;nulla}", 4);
+            //negatív
+            Console.WriteLine("{0:pozitív;negatív;nulla}", -4);
+            //nulla
+            Console.WriteLine("{0:pozitív;negatív;nulla}", 0);
+            Console.ReadKey();
+            
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Today);
+            Console.WriteLine(DateTime.UtcNow);
+            DateTime c = DateTime.Now;
+            Console.WriteLine(c.DayOfYear);
+            Console.ReadKey();
+            
+
+
+            int szuloev = 2004;
+            int eletkor = DateTime.Now.Year - szuloev;
+            Console.WriteLine(eletkor);
+            Console.WriteLine("");
+            Console.ReadKey();
+            
+            var ido = DateTime.Now;
+
+            Console.WriteLine("{0:d}", ido);
+            Console.WriteLine("{0:D}", ido);
+            Console.WriteLine("{0:t}", ido);
+            Console.WriteLine("{0:T}", ido);
+            Console.WriteLine("{0:f}", ido);
+            Console.WriteLine("{0:F}", ido);
+            Console.WriteLine("{0:g}", ido);
+            Console.WriteLine("{0:G}", ido);
+            Console.WriteLine("{0:M}", ido);
+            Console.WriteLine("{0:r}", ido);
+            Console.WriteLine("{0:Y}", ido);
+            Console.WriteLine("{0:yyyy-mm-dd, dddd tt hh:mm:ss}", ido);
+            Console.ReadKey();
+            */
+
+            int a = Math.Abs(-123);
+            Console.WriteLine(a);
+            double b =Math.Ceiling(1.23);
+            Console.WriteLine(b);
+            double c = Math.Floor(1.23);
+            Console.WriteLine(c);
+            double d = Math.Round(1.5333333,2);
+            Console.WriteLine(d);
+            Console.ReadKey();
+            
+
+           
+        }
+    }
+}
