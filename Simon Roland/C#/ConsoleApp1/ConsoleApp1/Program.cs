6cc2eee47f4a15b9ece722189f5cd03f0c6b3be1
Simon Roland <roland.simon@katonaj-mkszig.hu>
2023-09-28 09:35:46 +0200

    orai

--- Simon Roland/C#/ConsoleApp1/ConsoleApp1/Program.cs
+++ Simon Roland/C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace ConsoleApp1
 {
@@ -94,7 +96,7 @@ namespace ConsoleApp1
             //Console.Clear();
             int f= 3;
             Console.WriteLine(f / 4);
-            Console.WriteLine((double)f / 4);*/
+            Console.WriteLine((double)f / 4);
             
             Console.WriteLine("Add meg a négyzet oldalát! ");
             int a = Convert.ToInt32(Console.ReadLine());
@@ -102,12 +104,112 @@ namespace ConsoleApp1
             int terulet = a * a;
             Console.WriteLine("A négyzet kerülete: "+ kerulet);
             Console.WriteLine("A négyzet területe: " + terulet);
+            
+            int ma = 21;
+            int tegnap = 18;
+            var szoveg = string.Format("Ma {0} fok volt, tegnap pedig {1}", ma, tegnap);
 
-            
+            Console.WriteLine(szoveg);
 
-            
-            
-            
+            Console.WriteLine("Így is lehet: {0}, {1}", ma, tegnap);
+
+            Console.WriteLine("Pénz: {0:C1}", 3148);
+            Console.WriteLine("Százalék: {0:P3}", 0.123456);
+            Console.WriteLine("Általános: {0:G}", 132354);
+            Console.WriteLine("Exponenciális: {0:E}", 132354);
+            Console.WriteLine("Hexa: {0:X}", 255);
+
+            double d = 1500.42;
+
+            //1500,4200
+            Console.WriteLine("{0:00.0000}", d);
+            //(1500),42
+            Console.WriteLine("{0:(#).##}", d);
+            //1500,4
+            Console.WriteLine("{0:0.0}", d);
+            //1 500
+            Console.WriteLine("{0:0,0}", d);
+            //150042%
+            Console.WriteLine("{0:0%}", d);
+            //[Foo  ]
+            Console.WriteLine("[{0,-5}]", "Foo");
+            //[  Foo]
+            Console.WriteLine("[{0,5}]", "Foo");
+
+            //pozitív
+            Console.WriteLine("{0:pozitív;negatív;nulla}", 4);
+            //negatív
+            Console.WriteLine("{0:pozitív;negatív;nulla}", -4);
+            //nulla
+            Console.WriteLine("{0:pozitív;negatív;nulla}", 0);
+
+            var elso = "c:\\teszt\\foo";
+            //picivel egyszerűbb:
+            var masodik = @"c:\teszt\foo";
+
+            string nev = "World";
+            string hello = $"Hello {nev}!";
+            Console.WriteLine(hello);
+           
+            DateTime a = DateTime.Now;
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Today);
+            Console.WriteLine(DateTime.UtcNow);
+            Console.WriteLine(a.DayOfYear);
+ 
+            int szl = 2013;
+            int eletkor = DateTime.Now.Year - szl;
+            Console.WriteLine("Jóska Pista {0} éves", eletkor);
+
+            var ido = DateTime.Now;
+
+            Console.WriteLine("{0:d}", ido);
+            Console.WriteLine("{0:D}", ido);
+            Console.WriteLine("{0:t}", ido);
+            Console.WriteLine("{0:T}", ido);
+            Console.WriteLine("{0:f}", ido);
+            Console.WriteLine("{0:F}", ido);
+            Console.WriteLine("{0:g}", ido);
+            Console.WriteLine("{0:G}", ido);
+            Console.WriteLine("{0:M}", ido);
+            Console.WriteLine("{0:r}", ido);
+            Console.WriteLine("{0:Y}", ido);
+            Console.WriteLine("{0:yyyy-mm-dd, dddd tt hh:mm:ss}", ido);
+
+            string teszt = "🐰";
+            Console.WriteLine("A szoveg hossza: {0}", teszt.Length);
+
+            var info = new StringInfo(teszt);
+            Console.WriteLine("A szoveg valodi hossza: {0}", info.LengthInTextElements);
+*/
+
+            int a = Math.Abs(-123);
+            Console.WriteLine(a);
+            double b = Math.Ceiling(1.23);
+            Console.WriteLine(b);
+            double c = Math.Floor(1.23);
+            Console.WriteLine(c);
+            double d = Math.Round(1.5333, 2);
+            Console.WriteLine(d);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             Console.ReadKey();
         }
     }
