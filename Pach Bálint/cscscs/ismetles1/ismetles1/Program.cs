1ca2ad7f4813561cec490025a44b47de2215e014
Pach Balint <balint.pach@katonaj-mkszig.hu>
2023-10-26 08:57:05 +0200

    ora

---
+++ Pach Bálint/cscscs/ismetles1/ismetles1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ismétlés1
+{
+    namespace PeldaValtozok
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Beep(900, 2000);
+                //10-es szamrendszerben megadott egesz
+                int egesz_szam = 42;
+                Console.WriteLine(egesz_szam);
+                //az f jelzés jelöli a fordító számára,
+                //hogy ez egy float típus
+                float lebegopontos = 3.14f;
+                Console.WriteLine(lebegopontos);
+                //double esetén nem kell külön jelölni
+                double d = 1124.333;
+                Console.WriteLine(d);
+                //egész szám hexadecimális formában
+                int hexa = 0xff;
+                Console.WriteLine(hexa);
+                //hosszú egész oktális formátumban
+                long okta = 07123235;
+                Console.WriteLine(okta);
+                //decimal típus esetén m betű jelzi, hogy a szám egy decimal típus
+                decimal penz = 1224.3m;
+                Console.WriteLine(penz);
+
+                //A fordító a változó típusának string-et fog adni.
+                var valtozo = "Ez egy szöveg";
+                Console.WriteLine(valtozo);
+
+                //futtatás közben fog eldőlni a     típus.
+                //az eredmény típus szöveg lesz. A 44 szöveggé fog konvertálódni
+                dynamic dinamikus = "Ez egy " + 44;
+                Console.WriteLine(dinamikus);
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
