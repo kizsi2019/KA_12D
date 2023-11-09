f5b38cd7162f737343c95cb78519bbb0e1227a48
Szabo Levente Tamas <leventetamas.szabo@katonaj-mkszig.hu>
2023-11-09 14:25:29 +0100

    óraimunkás

---
+++ Szabó Levente Tamás/c#/órai 13.1 listás 2/órai 13.1 listás 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace órai_13._1_listás_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(12);
+            list.Add(43);
+            list.Add(2);
+            list.Insert(1, 31);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Szabó Levente Tamás/c#/órai 13.2 listások/órai 13.2 listások/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace órai_13._2_listások
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() {10,23,4};
+            list.Remove(23);
+            List<int> list2 = new List<int>() {10,23,4,30};
+            list2.RemoveAt(2);
+            Console.ReadKey();
+        }
+    }
+}
