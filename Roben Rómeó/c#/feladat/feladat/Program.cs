38e6dedf107973d1957164fe3ea34fd0a4fe7bc4
Roben Romeo <romeo.roben@katonaj-mkszig.hu>
2023-11-09 14:23:30 +0100

    bhjfdfdfdfhji

---
+++ Roben Rómeó/c#/feladat/feladat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("add meg a neved:");
+            string nev = Console.ReadLine();
+            do
+            {
+                List<int> átlag = new List<int>();
+                Console.WriteLine("add meg a jegyed");
+                double szamok = Console.ReadLine();
+            }
+        }
+    }
+}
