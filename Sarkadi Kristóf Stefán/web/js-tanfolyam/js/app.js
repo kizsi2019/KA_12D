80c5f1cd53073b7fc1ac9a7d302f190d04161a5b
Sarkadi Kristof Stefan <kristofstefan.sarkadi@katonaj-mkszig.hu>
2023-11-17 09:38:08 +0100

    kockajatek

--- Sarkadi Kristóf Stefán/web/js-tanfolyam/js/app.js
+++ Sarkadi Kristóf Stefán/web/js-tanfolyam/js/app.js
@@ -15,13 +15,17 @@ var pontszamok, korPontszamok;
 
 pontszam1 = [0,0]
 korPontszamok = 0;
+aktivJatekos = 0;
 
-aktivJatekos = 1;
-
-//document.querySelector("#current-").textContent = kocka;
-//var nev = document.querySelector("#name-aktivJatekos").textContent = '<u>' + kocka + '</u>';
+//document.querySelector("#current-" + aktivjatekos).textContent = kocka;
+//var nev = document.querySelector("#name-aktivJatekos").textContent;
+//console.log();
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
   //kell egy veletlen szám
@@ -33,6 +37,32 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
   kockaDOM.src = 'img/dice-' + kocka+ '.png'
 
   //körben elért pontszám frissítése, ha nem 1-et dobunk
-
+  if (kocka !== 1){
+      korPontszam += kocka;
+      documanet.querySelector('#current-' + aktivJatekos).textContent = korPontszam;
+  } else {
+      aktivJatekos === 0 ? aktivJatekos = 1 : aktivJatekos = 0;
+      korPontszam = 0;
+      
+      document.getElementById('current-0').textContent = '0';
+      document.getElementById('current-1').textContent = '1';
+      
+      document.getElementById('player-0-panel').classList.toggle('active');
+      document.getElementById('player-1-panel').classList.toggle('active');
+      
+      document.querySelector('.dice').style.display = 'none';
+  }
 
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function){
+    pontszamok[aktivJatekos];
+    document.querySelector('#score-' + aktivJatekos).textContent = pontszamok[aktivJatekos]
+    if (pontszamok[aktivJatekos] >= 20) {
+        document.querySelector('#name-' + aktivJatekos.textContent = 'Győztes');
+        document.querySelector('#name-' + aktivJatekos + 'panel').classList.add('#winner');
+        document.querySelector('#name-' + aktivJatekos + 'panel').classList.remove('active');
+    } else {
+        kovetkezoJatekos();
+    }
+}
