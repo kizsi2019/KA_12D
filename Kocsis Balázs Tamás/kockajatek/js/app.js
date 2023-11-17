41aa2f1cd7a4f22e0544bc1ddb0b5c15e9dd461b
Kocsis Balazs Tamas <balazstamas.kocsis@katonaj-mkszig.hu>
2023-11-17 09:38:04 +0100

    1117

--- Kocsis Balázs Tamás/kockajatek/js/app.js
+++ Kocsis Balázs Tamás/kockajatek/js/app.js
@@ -23,10 +23,55 @@ aktivJatekos = 0;
 //document.querySelector('#current-' + aktivJatekos).textContent = '<u>' + kocka + '</u>';
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
-  var kocka = Math.floor(Math.random() * 6) + 1;
-  var kockaDOM = document.querySelector('.dice');
-  kockaDOM.style.display = 'block';
-  kockaDOM.src = 'img/dice-' + kocka + '.png';
-});
+    kocka = Math.floor(Math.random() * 6) + 1;
+    
+    var kockaDOM = document.querySelector('.dice');
+    kockaDOM.style.display = 'block';
+    kockaDOM.src = 'img/dice-' + kocka + '.png';
+
+    if (kocka !== 1) {
+        korPontszam += kocka;
+        document.querySelector('#current-' + aktivJatekos).textContent = korPontszam;
+    }
+    else {
+        //kovetkezo jatekos
+        kovetkezoJatekos();
+        }
+    });
+
+    document.querySelector('.btn-hold').addEventListener('click', function() {
+        pontszamok[aktivJatekos] += korPontszam;
+    
+        document.querySelector('#score-' + aktivJatekos).textContent = pontszamok[aktivJatekos];
+    
+        if (pontszamok[aktivJatekos] >= 20) {
+            document.querySelector('#name-' + aktivJatekos).textContent = 'Győztes!';
+            document.querySelector('#name-' + aktivJatekos + '-panel').classList.add('winner');
+            document.querySelector('#name-' + aktivJatekos + '-panel').classList.remove('active');
+    
+        }
+        else {
+            kovetkezoJatekos();
+        }
+    });
+
+    //kovetkezo jatekosű
+    function kovetkezoJatekos() {
+        
+        aktivJatekos === 0 ? aktivJatekos = 1 : aktivJatekos = 0;
+        korPontszam = 0;
+    
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+    
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+    
+        document.querySelector('.dice').style.display = 'none';
+    }
