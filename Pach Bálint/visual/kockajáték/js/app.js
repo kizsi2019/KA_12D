b4f09621d2da47d5843b03f035d36bb8f2e4c585
Pach Balint <balint.pach@katonaj-mkszig.hu>
2023-11-17 09:37:57 +0100

    xd

---
+++ Pach Bálint/visual/kockajáték/js/app.js
@@ -0,0 +1,54 @@
+/*
+Játék szabályok:
+
+- A játék 2 szereplős és körökre osztott
+- Minden egyes körben az adott játékos dob a kockával, ahányszor csak szeretne. A dobások eredménye hozzáadódik a játékos adott körben
+  elért pontszámához, ami értelem szerűen minden körben nulláról indul.
+- Ha az aktuális játékos 1-et dob, akkor az összes addigi pontja elveszik, és átadja a dobás jogát a következő játékosnak.
+- A játékos választhatja a 'Megtartom' gombot is. Ebben az esetben az adott körben elért pontok száma, hozzáadódik a játékos összes
+  pontszámához. Majd a dobás joga a másik játékosra száll.
+- Az a játékos nyer, aki előbb eléri a 100 pontot.  
+
+*/
+
+var pontszamok, korPontszam, aktivjatekos;
+
+pontszamok = [0,0];
+korPontszam = 0;
+aktivjatekos = 1;
+
+
+//document:.querySelector(*#current-" + aktivjatekos).textcontent = ‘<u>’ + kocka + '</u>"s
+
+document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textcontent = "0";
+document.getElementById("current-0").textcontent = "0";
+document.getElementById("score-1").textcontent = "0";
+document.getElementById("current-0").textcontent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function(){
+  //kell egy veletlen szm
+  var kocka = Math.floor(Math.random() * 6) + 1;
+
+  // erednény megjelenitése
+  var kockaDOM = document.querySelector(".dice");
+  kockaDOM.style.display = "block";
+  kockaDOM.src = "img/dice-" + kocka + ".png";
+
+  if (kocka !== 1){
+    korPontszam += kocka;
+    document.querySelector('#current-' + aktivjatekos).textContent =  korPontszam;
+  }
+  else{
+    aktivjatekos === 0 ? aktivjatekos = 1 : aktivjatekos = 0;
+    korPontszam = 0;
+
+    document.getElementById('current-0'.textContent = '0');
+    document.getElementById('current-1'.textContent = '0');
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    document.querySelector(".dice").style.display = "none";
+  }
+  
+})
