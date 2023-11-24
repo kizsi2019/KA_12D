158466fdbd5bc38ba0757ef14fc3ef86e1f1e35b
Rozsahegyi Akos <akos.rozsahegyi@katonaj-mkszig.hu>
2023-11-24 09:32:27 +0100

    Kocka 2023-11-24

--- Rózsahegyi Ákos/JS/js-tanfolyam/js/app.js
+++ Rózsahegyi Ákos/JS/js-tanfolyam/js/app.js
@@ -12,19 +12,9 @@ Játék szabályok:
 
 var pontszamok, korpontszam, aktivJatekos;
 
-pontszamok = [0,0];
-korpontszam = 0;
-aktivJatekos = 0;
+init();
 
-// document.querySelector('#current-' + aktivJatekos).textContent = kocka;
-// document.querySelector('#current-' + aktivJatekos).textContent = '<u>' + kocka + '</u>';
-
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
+// dobás gomb kezelője
 document.querySelector('.btn-roll').addEventListener('click', function() {
   // kell egy véletlen szám
   var kocka = Math.floor(Math.random() * 6) + 1;
@@ -48,6 +38,9 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 document.querySelector('.btn-hold').addEventListener('click', function() {
     // összes pontszám frissítése a kódban
     pontszamok[aktivJatekos] += korpontszam;
+
+    // összes pontszám frissítése a felületen (UI)
+    document.querySelector('#score-' + aktivJatekos).textContent = pontszamok[aktivJatekos];
     
     // nyert a játékos?
     if (pontszamok[aktivJatekos] >= 20) {
@@ -67,8 +60,32 @@ function kovetkezoJatekos() {
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
       
-    document.getElementById('.player-0-panel').classList.toggle('active');
-    document.getElementById('.player-1-panel').classList.toggle('active');
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
       
     document.querySelector('.dice').style.display = 'none';
+}
+
+// új játék létrehozása
+document.querySelector('.btn-new').addEventListener('click', init);
+
+// init
+function init() {
+  pontszamok = [0,0];
+  korpontszam = 0;
+  aktivJatekos = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.getElementById('name-0').textContent = 'Frodó';
+  document.getElementById('name-1').textContent = 'Samu';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
 }
