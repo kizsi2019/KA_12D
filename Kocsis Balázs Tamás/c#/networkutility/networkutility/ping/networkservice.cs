f1b667e4f25b2f43d839b2e63ecb722e037cb1eb
Kocsis Balazs Tamas <balazstamas.kocsis@katonaj-mkszig.hu>
2024-01-10 12:35:37 +0100

    asgaga

---
+++ Kocsis Balázs Tamás/c#/networkutility/networkutility/ping/networkservice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace networkutility.ping
+{
+    internal class networkservice
+    {
+        public string SendPing()
+        {
+            return "Success: Ping sent!";
+        }
+
+        public int PingTimeout(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
