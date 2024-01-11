bbcfb3a10364209f3a0fe5d78806d5c7be0de4fb
unknown <peter.takacs@katonaj-mkszig.hu>
2024-01-11 10:33:40 +0100

    asd

---
+++ Takács Péter/c#/00P1/00P1/Ember.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00P1
+{
+    internal class Ember
+    {
+        public Ember(string n, int é, int sz)
+        {
+            Név = n;
+            Életkor = é;
+            Irányítószám = sz;
+        }
+        private string név;
+        public string Név
+        {
+            get
+            {
+                return név;
+            }
+            set
+            {
+                if (value.Length != 0) név = value;
+                else Exception("A név mező nem lehet üres!");
+            }
+        }
+        private int életkor;
+        public int Életkor
+        {
+            get
+            {
+                return életkor;
+            }
+            set
+            {
+                if (value >= 0) életkor = 0;
+                else Exception("Az életkor nem lehet negatív! ");
+            }
+        }
+        private int irányítószám;
+        public int Irányítószám
+        {
+            get
+            {
+                return irányítószám;
+            }
+            set
+            {
+                if (value.ToString().Length == 4)
+                {
+                    irányítószám = value;
+                }
+                else Exception("Az irányítószám négy karakter! ");
+            }
+        }
+        private void Exception(string s)
+        {
+            throw new Exception(s);
+        }
+    }
+}
