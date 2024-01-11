904c930dba5e55d4ed5bff7ef7fe99f55559d21c
Simon Roland <roland.simon@katonaj-mkszig.hu>
2024-01-11 09:56:51 +0100

    asd

--- Simon Roland/teszt/NetworkUtility/NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
+++ Simon Roland/teszt/NetworkUtility/NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -3,31 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NetworkUtility.T;
+using Xunit;
+using FluentAssertions;
+using NetworkUtility.Ping;
 
 
 namespace NetworkUtility.Tests.PingTests
 {
-    internal class NetworkServiceTests
+    public class NetworkServiceTests
     {
         [Fact]
         public void NetworkService_SendPing_ReturnString()
         {
-            var pingService = new NetworkServiceTests();
-            var result = pingService.SendPing();
+            var pingService = new NetworkService();
+            var result = pingService.PingTimeout();
             result.Should().Be("Success: Ping sent!");
             result.Should().NotBeNullOrWhiteSpace();
             result.Should().Contain("Ping", Exactly.Once());
         }
         [Theory]
-        [InlineData[1,1,2]]
-        [InlineData[2,2,4]]
+        [InlineData(1,1,2)]
+        [InlineData(2,2,4)]
         public void NetworkService_ReceivePing_ReturnInt(int a, int b, int expected)
         {
-            var pingService = new NetworkServiceTests();
-            var result = pingService.NetworkService_SendPing_ReturnInt(a, b);
+            var pingService = new NetworkService();
+            var result = pingService.PingTimeout(a, b);
             result.Should().Be(expected);
-            result.Should().NotBeGreaterThanOrEqualTo(2);
+            result.Should().BeGreaterThanOrEqualTo(2);
             result.Should().NotBeInRange(-1000, 0);
         }
     }
