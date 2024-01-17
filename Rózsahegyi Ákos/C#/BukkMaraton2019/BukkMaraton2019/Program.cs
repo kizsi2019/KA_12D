08dbd4ddaddb8137651e4a7e26315992ff32cbdc
Rozsahegyi Akos <akos.rozsahegyi@katonaj-mkszig.hu>
2024-01-17 12:35:09 +0100

    Bukkm 2023-01-17

---
+++ Rózsahegyi Ákos/C#/BukkMaraton2019/BukkMaraton2019/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BukkMaraton2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3. feladat
+            List<Versenyzo> versenyzok = new List<Versenyzo>();
+            foreach (var sor in File.ReadAllLines("bukkm2019.txt").Skip(1))
+            {
+                versenyzok.Add(new Versenyzo(sor));
+            }
+            Console.WriteLine($"4. feladat: Versenytávot nem teljesítők: {(1 - versenyzok.Count / 691.0) * 100}%");
+            Console.ReadKey();
+        }
+    }
+}
