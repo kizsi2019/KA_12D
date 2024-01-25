40bd59821a7c7fa9f63cc32c62a3299391981753
DubeczDavid <davidraymundo.dubecz@katonaj-mkszig.hu>
2024-01-25 14:08:26 +0100

    tanarurvanferrarija?

--- Dubecz David Raymundo/C#/OrvosiNobeldijasokGUI/OrvosiNobeldijasokGUI/MainWindow.xaml.cs
+++ Dubecz David Raymundo/C#/OrvosiNobeldijasokGUI/OrvosiNobeldijasokGUI/MainWindow.xaml.cs
@@ -9,9 +9,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.IO;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Security.AccessControl;
 
 namespace OrvosiNobeldijasokGUI
 {
@@ -23,6 +25,36 @@ namespace OrvosiNobeldijasokGUI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BntMentes_Click(object sender, RoutedEventArgs e)
+        {
+            if (TbEv.Text != "" && TbNev.Text != "" && TbSzulEsHal.Text != "" && TbOrszag.Text != "")
+            {
+                if (int.Parse(TbEv.Text) <= 1989)
+                {
+                    MessageBox.Show("Hiba, az evszam nem jo!", "Mentes");
+                }
+                try
+                {
+                    List<string> ki = new List<string>();
+                    ki.Add("Év;Név;SzületésHalálozás;Országkód");
+                    ki.Add($"{TbEv.Text},{TbNev},{TbSzulEsHal},{TbOrszag}");
+                    File.WriteAllLines("uj_dijazott.txt", ki);
+                    TbEv.Text = "";
+                    TbNev.Text= "";
+                    TbSzulEsHal.Text = "";
+                    TbOrszag.Text = "";
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Hiba az allomany irasanal");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Toltse ki ezt a mezot");
+            }
         }
     }
 }
