68580411b5099c9d384fc84e757c7c4ed1c0fbce
DubeczDavid <davidraymundo.dubecz@katonaj-mkszig.hu>
2024-01-25 09:13:27 +0100

    lol

--- Dubecz David Raymundo/Java/js-tanfolyam/js/app.js
+++ Dubecz David Raymundo/Java/js-tanfolyam/js/app.js
@@ -1,6 +1,6 @@
 /*
 Játék szabályok:
-
+ 
 - A játék 2 szereplős és körökre osztott
 - Minden egyes körben az adott játékos dob a kockával, ahányszor csak szeretne. A dobások eredménye hozzáadódik a játékos adott körben
   elért pontszámához, ami értelem szerűen minden körben nulláról indul.
@@ -9,21 +9,21 @@ Játék szabályok:
   pontszámához. Majd a dobás joga a másik játékosra száll.
 - Az a játékos nyer, aki előbb eléri a 100 pontot.  
 */
-
+ 
 var pontszamok, korpontszam, aktivJatekos;
-
+ 
 init();
-
+ 
 // dobás gomb kezelője
 document.querySelector('.btn-roll').addEventListener('click', function() {
   // kell egy véletlen szám
   var kocka = Math.floor(Math.random() * 6) + 1;
-
+ 
   // eredmény megjelenítése
   var kockaDOM = document.querySelector('.dice');
   kockaDOM.style.display = 'block';
   kockaDOM.src = 'img/dice-' + kocka + '.png';
-    
+   
   // körben elért pontszám frissítése, ha nem 1-et dobunk
   if (kocka !== 1) {
       korpontszam += kocka;
@@ -33,15 +33,15 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
       kovetkezoJatekos();
   }
 });
-
+ 
 // megtartom gomb eseménykezelője
 document.querySelector('.btn-hold').addEventListener('click', function() {
     // összes pontszám frissítése a kódban
     pontszamok[aktivJatekos] += korpontszam;
-
+ 
     // összes pontszám frissítése a felületen (UI)
     document.querySelector('#score-' + aktivJatekos).textContent = pontszamok[aktivJatekos];
-    
+   
     // nyert a játékos?
     if (pontszamok[aktivJatekos] >= 20) {
         document.querySelector('#name-' + aktivJatekos).textContent = 'Győztes!';
@@ -52,35 +52,35 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         kovetkezoJatekos();
     }
 });
-
+ 
 function kovetkezoJatekos() {
     aktivJatekos === 0 ? aktivJatekos = 1 : aktivJatekos = 0;
     korpontszam = 0;
-      
+     
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
-      
+     
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
-      
+     
     document.querySelector('.dice').style.display = 'none';
 }
-
+ 
 // új játék létrehozása
 document.querySelector('.btn-new').addEventListener('click', init);
-
+ 
 // init
 function init() {
   pontszamok = [0,0];
   korpontszam = 0;
   aktivJatekos = 0;
-
+ 
   document.querySelector('.dice').style.display = 'none';
   document.getElementById('score-0').textContent = '0';
   document.getElementById('current-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
   document.getElementById('current-1').textContent = '0';
-
+ 
   document.getElementById('name-0').textContent = 'Frodó';
   document.getElementById('name-1').textContent = 'Samu';
   document.querySelector('.player-0-panel').classList.remove('winner');
