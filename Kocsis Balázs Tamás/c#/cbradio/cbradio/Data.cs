17be78e8725023019ff1958c404de7697f1e4dcb
Kocsis Balazs Tamas <balazstamas.kocsis@katonaj-mkszig.hu>
2024-02-08 14:21:07 +0100

    0208

---
+++ Kocsis Balázs Tamás/c#/cbradio/cbradio/Data.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace cbradio
+{
+    internal class Data
+    {
+        public int Ora {get; private set;}
+        public int Perc { get; private set;}
+        public int Kezdes { get; private set; }
+        public int Adatdb { get; private set;}
+        public string Nev { get; private set;}
+
+        public Data(string sor)
+        {
+            string[] m = sor.Split(';');
+            Ora = int.Parse(m[0]);
+            Perc = int.Parse(m[1]);
+            Adatdb = int.Parse(m[2]);
+            Nev = (m[3]);
+        }
+
+        public int AtszamolPercre()
+        {
+            return Ora * 60 + Perc;
+        }
+
+        
+
+
+    }
+}
