9a13a5fabd34d24a89e4987d4f0890a4fcbe67e1
Kocsis Balazs Tamas <balazstamas.kocsis@katonaj-mkszig.hu>
2024-02-09 09:32:36 +0100

    0209

---
+++ Kocsis Balázs Tamás/javascript/j39/script.js
@@ -0,0 +1,28 @@
+var szamlalo = 0;
+function leptet(){
+    var szamlalo = 0;
+    return function(){
+        szamlalo++;
+        console.log(szamlalo);
+    }
+}
+
+var hozzaad = leptet();
+hozzaad();
+hozzaad();
+hozzaad();
+
+var leptet = (
+    function(){
+        var szamlalo = 0;
+
+        return function(){
+            szamlalo++;
+            console.log(szamlalo);
+        }
+    }
+)();
+
+leptet();
+leptet();
+leptet();
