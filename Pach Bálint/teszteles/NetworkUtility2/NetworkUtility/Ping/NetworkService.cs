2f3d2e9d1464dd16532536e619c177ce4619744c
Pach Balint <balint.pach@katonaj-mkszig.hu>
2024-02-15 08:44:20 +0100

    geco

--- Pach Bálint/teszteles/NetworkUtility2/NetworkUtility/Ping/NetworkService.cs
+++ Pach Bálint/teszteles/NetworkUtility2/NetworkUtility/Ping/NetworkService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,18 @@ namespace NetworkUtility.Ping
         {
             return "Success: Ping sent";
         }
-
+        public DateTime LastPingDate()
+        {
+            return DateTime.Now;
+        }
+        public PingOptions GetPingOptions()
+        {
+            return new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+        }
         public int PingTimeout(int a, int b)
         {
             return a + b;
