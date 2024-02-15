fb61fc655b844aaf91cd306d36da5d576a3ff9cd
Sarkadi Kristof <kristofstefan.sarkadi@katonaj-mkszig.hu>
2024-02-15 08:44:30 +0100

    UTPO

--- Sarkadi Kristóf Stefán/testeles/NetworkUtility/NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
+++ Sarkadi Kristóf Stefán/testeles/NetworkUtility/NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using NetworkUtility.Ping;
 using Xunit;
 using FluentAssertions;
+using FluentAssertions.Extensions;
+using System.Net.NetworkInformation;
 
 namespace NetworkUtility.Tests.PingTests
 {
@@ -33,9 +35,11 @@ namespace NetworkUtility.Tests.PingTests
             result.Should().NotBeNullOrWhiteSpace();
             result.Should().Contain("Ping", Exactly.Once());
         }
+
         [Theory]
         [InlineData(1,1,2)]
         [InlineData(2,2,4)]
+
         public void NetworkService_PingTimeout_ReturnInt(int a, int b, int expected)
         {
             // Arrange
@@ -48,6 +52,39 @@ namespace NetworkUtility.Tests.PingTests
             result.Should().Be(expected);
             result.Should().BeGreaterThanOrEqualTo(2);
             result.Should().NotBeInRange(-1000, 0);
+        }
+
+        [Fact]
+        public void NetworkService_LastPingDate_ReturnDate()
+        {
+            // Arrange
+            
+
+            // Act
+            var result = _pingService.LastPingDate();
+
+            // Assert
+            result.Should().BeAfter(1.January(2010));
+            result.Should().BeBefore(1.January(2030));
+        }
+
+        [Fact]
+        public void NetworkService_SendPing_ReturnOtions()
+        {
+            // Arrange
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            // Act
+            var result = _pingService.GetPingOptions();
+
+            // Assert
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
         }
     }
 }
