7051ec901d6082a0d4e387a7710d9923474f86ef
SoponyaiAron <aron.soponyai@katonaj-mkszig.hu>
2024-02-22 10:07:00 +0100

    asd

---
+++ Soponyai Áron/c#/TestProject1/tesztek/7feladat.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BukkMaraton.Tests.Tesztek
+{
+    public class feladat7
+    {
+        [Fact]
+        public void GyoztesFelnottFerfiRovidTav_KivalasztasHelyes()
+        {
+            // Arrange
+            var versenyzok = new List<Versenyzo>
+        {
+            new Versenyzo { Tav = "Hosszú", Kategoria = "ff", Ido = 120, Rajtszam = 1, Nev = "Teszt Elek", Egyesulet = "Test Egyesület" },
+            new Versenyzo { Tav = "Rövid", Kategoria = "ff", Ido = 110, Rajtszam = 2, Nev = "Kovács János", Egyesulet = "Példa Egyesület" },
+            new Versenyzo { Tav = "Rövid", Kategoria = "ff", Ido = 115, Rajtszam = 3, Nev = "Nagy Gábor", Egyesulet = "Példa Egyesület" },
+            new Versenyzo { Tav = "Rövid", Kategoria = "ff", Ido = 105, Rajtszam = 4, Nev = "Kiss István", Egyesulet = "Példa Egyesület" }
+        };
+            var elvartGyoztes = new Versenyzo { Tav = "Rövid", Kategoria = "ff", Ido = 105, Rajtszam = 4, Nev = "Kiss István", Egyesulet = "Példa Egyesület" };
+
+            // Act
+            Versenyzo gyoztesFelnottFerfiRovidTav = null;
+            foreach (var item in versenyzok)
+            {
+                if (item.Tav == "Rövid" && item.Kategoria == "ff")
+                {
+                    if (gyoztesFelnottFerfiRovidTav == null)
+                    {
+                        gyoztesFelnottFerfiRovidTav = item;
+                    }
+                    else
+                    {
+                        if (item.Ido < gyoztesFelnottFerfiRovidTav.Ido)
+                        {
+                            gyoztesFelnottFerfiRovidTav = item;
+                        }
+                    }
+                }
+            }
+
+            // Assert
+            gyoztesFelnottFerfiRovidTav.Should().NotBeNull("mivel van győztes felnőtt férfi rövid távon");
+            gyoztesFelnottFerfiRovidTav.Should().BeEquivalentTo(elvartGyoztes, options => options.ExcludingMissingMembers(), "a győztesnek meg kell egyeznie az elvárt értékekkel");
+        }
+    }
+}
---
+++ Soponyai Áron/c#/delegacio/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace delegacio
+{
+    internal class Program
+    {
+        /*delegate void VoidDelegate();
+        static void Metodus()
+        {
+            Console.WriteLine("A metodus lefutott!");
+        }
+        static void Main(string[] args)
+        {
+            VoidDelegate vd = new VoidDelegate(Metodus);
+            vd();
+            Console.ReadKey();  
+        }
+
+        delegate void Beléptetés(string str);
+        static void Üdvözlés(string név)
+        {
+            Console.WriteLine("Üdvözzöllek {0}!", név);
+        }
+        static void Üzenet(string név)
+        {
+            Console.WriteLine("Kedves {0}, köszönjük a belépésedet a rendszerbe...", név);
+        }
+        static void Main(string[] args)
+        {
+            Beléptetés b = new Beléptetés(Üdvözlés) + new Beléptetés(Üzenet);
+            Console.Write("Add meg a nevedet: ");
+            string név = Console.ReadLine();
+            b(név);
+        }
+        */
+
+        class Esemény : EventArgs
+        {
+            public string üzenet;
+            //aki nem tudja, mit csinál a konstruktor lapozzon vissza!
+            public Esemény(string str) : base()
+            {
+                üzenet = str;
+            }
+        }
+
+        class Szám
+        {
+            public delegate void EseménykezelőDelegate(object o, Esemény e);
+            public event EseménykezelőDelegate ÁllapotváltozásEsemény;
+            int szam = 0;
+            public int Szam
+            {
+                get { return szam; }
+                set
+                {
+                    szam = value;
+                    ÁllapotVáltozás();
+                }
+            }
+            private void ÁllapotVáltozás()
+            {
+                if (ÁllapotváltozásEsemény != null)
+                    ÁllapotváltozásEsemény(this, new Esemény("A szám megváltozott!"));
+                //a this-szel átadja az osztály saját magát
+            }
+        }
+        class Programm
+        {
+            static void EseményKezelés(object eseménytkiváltóosztály, Esemény e)
+            {
+                Console.WriteLine(e.üzenet);
+            }
+            static void Main(string[] args)
+            {
+                Szám sz = new Szám();
+                sz.ÁllapotváltozásEsemény += EseményKezelés; //feliratkozunk az eseményre
+                sz.Szam = 21; //kiváltjuk az eseményt;
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
