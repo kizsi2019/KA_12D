38db0cfd0acb63b85f24f87b42ac309d6b229d1e
DubeczDavid <davidraymundo.dubecz@katonaj-mkszig.hu>
2024-02-22 10:02:35 +0100

    v888

---
+++ Dubecz David Raymundo/software tesztelés/NetworkUtility/NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using FakeItEasy;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkUtility.Ping;
+using Xunit;
+using FluentAssertions;
+using System.Runtime.CompilerServices;
+using FluentAssertions.Extensions;
+using System.Net.NetworkInformation;
+
+namespace NetworkUtility.Tests.PingTests
+{
+    public class NetworkServiceTests
+    {
+        private readonly NetworkService _pingService;
+        private readonly DNS2.IDNS _dNS;
+
+        public NetworkServiceTests()
+        {
+            // Dependencies
+            _dNS = A.Fake<DNS2.IDNS>();
+            
+            // SUT - System Under Test
+            _pingService = new NetworkService(_dNS);
+        }
+
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            // Arrange
+            A.CallTo(() => _dNS.SendDNS()).Returns(true);
+
+            // Act
+            var result = _pingService.SendPing();
+
+            // Assert
+            result.Should().Be("Success: Ping sent");
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Contain("Ping", Exactly.Once());
+        }
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(2,2,4)]
+        public void NetworkService_PingTimeout_ReturnInt(int a, int b, int expected)
+        {
+            // Arrange
+
+            // Act
+            var result = _pingService.PingTimeout(a, b);
+
+            // Assert
+            result.Should().Be(expected);
+            result.Should().BeGreaterThanOrEqualTo(2);
+            result.Should().NotBeInRange(-1000, 0);
+        }
+
+        [Fact]
+        public void NetworkService_LastPingDate_ReturnDate()
+        {
+            // Arrange
+
+            // Act
+            var result = _pingService.LastPingDate();
+
+            // Assert
+            result.Should().BeAfter(1.January(2010));
+            result.Should().BeBefore(1.January(2030));
+        }
+
+        [Fact]
+        public void NetworkService_GetPingOptions_ReturnsObject()
+        {
+            // Arrange
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            // Act
+            var result = _pingService.GetPingOptions();
+
+            // Assert WARNING: "Be" careful
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
+        }
+
+        [Fact]
+        public void NetworkService_MostRecentPings_ReturnsObject()
+        {
+            // Arrange
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            // Act
+            var result = _pingService.MostRecentPings();
+
+            // Assert WARNING: "Be" careful
+            result.Should().BeOfType<PingOptions[]>();
+            result.Should().ContainEquivalentOf(expected);
+            result.Should().Contain(x => x.DontFragment == true);
+        }
+    }
+}
