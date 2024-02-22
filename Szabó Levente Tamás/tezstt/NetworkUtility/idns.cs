6d25856794ae10e0a811bd57bf9dafdd8e34ace7
Szabo Levente Tamas <leventetamas.szabo@katonaj-mkszig.hu>
2024-02-22 10:06:10 +0100

    02.22

---
+++ Szabó Levente Tamás/tezstt/NetworkUtility/idns.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collection.Generic;
+using System.Linq;
+using.System.Text;
+using System.Threading.Task;
+
+namespace idns
+{
+    public class DNSService : IDns
+    {
+        public bool SendDNS()
+        {
+            return true;
+        }
+    }
+}
