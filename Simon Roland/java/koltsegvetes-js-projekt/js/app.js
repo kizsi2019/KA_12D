c1dce33ed5b28a90308b2e128a9af4034b40368c
Simon Roland <roland.simon@katonaj-mkszig.hu>
2024-03-01 09:33:21 +0100

    asd

---
+++ Simon Roland/java/koltsegvetes-js-projekt/js/app.js
@@ -0,0 +1,30 @@
+//Koltsegvetesvezerlo
+var koltsegvetesvezerlo = (function(){
+    
+})();
+//feluletvezerlo
+var feluletvezerlo = (function(){
+
+})();
+
+// alkalmazasvezerlo
+var vezerlo = (function(koltsegvetesvez, feluletvez){
+document.querySelector('.hozzaad__gomb'),addEventListener('click', function(){
+    // 1. bevitt adatok megszerzese 
+    
+    // 2. adatok atadasa a koltsegvetesvezerlo modulnak
+    
+    // 3. megjelenites ui-n
+    
+    // 4. koltsegvetes ujraszamolasa
+
+    // 5. osszeg megjelenitese a feluleten
+});
+document.addEventListener('keydown', function(event){
+    if (event.keyCode === 13){
+        console.log('enter')
+    }
+
+});
+
+})(koltsegvetesvezerlo, feluletvezerlo)
