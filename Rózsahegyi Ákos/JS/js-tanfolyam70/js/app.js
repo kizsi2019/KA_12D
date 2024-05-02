c64a6b2e4ce5b712cc0f5e67dd6a0669711ce6a5
Rozsahegyi Akos <akos.rozsahegyi@katonaj-mkszig.hu>
2024-05-02 10:24:36 +0200

    JStanfolyam 2024-05-02

---
+++ Rózsahegyi Ákos/JS/js-tanfolyam70/js/app.js
@@ -0,0 +1,70 @@
+// let, const
+// ES5
+
+var nev5 = 'Vicc Elek';
+var kor5 = 40;
+
+nev5 = 'Abbanapillanatban Debrüi'
+
+console.log(nev5);
+
+function nyelvVizsga5(siker) {
+    if (siker) {
+        var keresztnev = 'Gizi';
+        var szuletesiEv = 1980;
+        console.log(keresztnev + '(születési év: ' + szuletesiEv + ') sikeres volt a vizsga');
+    }
+}
+nyelvVizsga5(true);
+
+
+// ES6
+
+const nev6 = 'Fing Ottam';
+let kor6 = 40;
+
+// nev6 = 'Kiss Pista'; // Hibát dob
+// console.log(nev6);
+
+function nyelvVizsga6(siker) {
+    if (siker) {
+        var keresztnev = 'Trottyos';
+        const szuletesiEv = 1985;
+        console.log(keresztnev + '(születési év: ' + szuletesiEv + ') sikeres volt a vizsga');
+    }
+}
+nyelvVizsga6(true);
+
+/*
+let i = 9;
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+console.log(i);
+*/
+
+var i = 9;
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+console.log(i);
+
+
+////////////////////////////////////////////////////////////////
+// Blokkok és IIFE
+// ES6
+
+{
+    const a = 1;
+    let b = 2;
+    // var c = 3;
+}
+// console.log(a + b);
+
+// ES5
+
+(function() {
+    let c = 3;
+    console.log(c);
+})();
+console.log(c);
