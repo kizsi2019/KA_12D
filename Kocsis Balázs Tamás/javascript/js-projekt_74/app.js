71e278a5d0766cc19cff1c073a4c676b744b9154
Kocsis Balazs Tamas <balazstamas.kocsis@katonaj-mkszig.hu>
2024-05-16 12:28:43 +0200

    05162

---
+++ Kocsis Balázs Tamás/javascript/js-projekt_74/app.js
@@ -0,0 +1,217 @@
+// let es const
+//es 5 
+/*
+var nev5 = 'Teszt Elek';
+var kor5 = 40;
+
+nev5 = "Kiss Pista";
+
+//console.log(nev5);
+//es5
+function nyelvVizsga(siker){
+    if (siker){
+        var keresztnev = 'Ödön';
+        var szuletesiEv = 1980;
+        console.log(keresztnev + '(születési év: ' + szuletesiEv + ') sikeres volt a vizsga');
+    }
+}
+nyelvVizsga(true);
+
+//es6 
+
+const nev6 = 'Teszt Elek';
+let kor6 = 40;
+
+//nev6 = 'Kiss Pista';
+
+//console.log(nev6);
+
+function nyelvVizsga6(siker){
+    if(siker){
+        var keresztnev = 'Ödön';
+        const szuletesiEv = 1980;
+        console.log(keresztnev + '(Születési év: ' + szuletesiEv + ') sikeres volt a vizsga' );
+    }
+}
+//nyelvVizsga6(true);
+
+let i = 9;
+for (let i = 0; i < 5; i++){
+   // console.log(i);
+}
+
+//console.log(i);
+
+var f = 9;
+for (let f = 0; f < 5; f++){
+   // console.log(f);
+}
+//console.log(f);
+
+{
+    const a = 1;
+    let b = 2;
+    // var c = 3;
+
+}
+
+
+//console.log(a+b);
+
+(function(){
+    var c = 3;
+    console.log(c);
+})();
+
+// console.log(c);
+
+// stringek
+
+let vezetekNev = 'Teszt';
+let keresztNev = 'Elek';
+const szuletesiEv = 1980;
+
+function korSzam(ev) {
+    return 2024 - ev;
+}
+//es5
+console.log(vezetekNev + ' ' + keresztNev + ', szuletett ' + szuletesiEv + '-ben. Most ' + korSzam(szuletesiEv) + 'eves ');
+
+//es6
+
+console.log(`${vezeteknev} ${keresztNev}, szuletett ${szuletesiEv}-ben. Most ${korSzam(szuletesiEv)} eves `);
+
+const nev = `${vezetekNev} ${keresztNev}`;
+
+console.log(nev.startsWith('t'));
+
+console.log(nev.endsWith('ekeee'));
+
+console.log(nev.includes('ele'));
+
+console.log(vezetekNev.repeat(3));
+
+console.log(`${vezetekNev}`.repeat(3))
+
+// nyilt fuggvenyek
+
+const evek = [1970,1975,1954, 2010, 1981];
+
+//es5
+var korokES5 = evek.map(function(elem){
+    return 2024 - elem;
+});
+console.log(korokES5);
+
+let korokES6 = evek.map(elem => 2024 - elem);
+
+console.log(korokES6);
+
+korokES6 = evek.map((elem, index) => `Kor ${index}: ${index}: ${2024 - elem}.`);
+
+console.log(korokES6)
+
+korokES6 = evek.map((elem, index) => {
+    const aktEv = new Date().getFullYear();
+    const kor = aktEv - elem;
+    return `Kor ${index}: ${kor}`;
+});
+
+console.log(korokES6)
+
+const dobozES62 = {
+    szin: 'zöld',
+    pozicio: 1,
+    kattintsRam: () => {
+        document.querySelector('.green').addEventListener('click', () => {
+            var szoveg = 'Én vagyok a(z) ' + this.pozicio + '. doboz és a színem ' + this.szin + '.';
+            alert(szoveg);
+        })
+    }
+};
+
+dobozES62.kattintsRam();
+
+
+
+function Szemely(nev){
+    this.nev = nev;
+}
+
+Szemely.prototype.barataimES5 = function(haverok){
+    console.log(this.nev);
+    var obj = this;
+    var tomb = haverok.map(function(elem){
+        return obj.nev + 'baratja' + elem + '.';
+    });
+    console.log(tomb);
+}
+
+var haverok = ['joska', 'pista', 'odi'];
+new Szemely('Geza').barataimES5(haverok);
+
+
+Szemely.prototype.barataimES5 = function(haverok) {
+    const tomb = haverok.map(elem => `${this.nev} baratja ${elem}.`);
+    console.log(tomb);
+}
+
+new Szemely('Geza').barataimES5(haverok);
+*/
+
+////////////// Destrukturalas ////////////////////////////////////////////////////////////////////////
+
+//es5
+
+var odon = ['Odon', 50];
+var nev = odon[0];
+var kor = odon[1];
+
+console.log(nev);
+console.log(kor);
+
+//es6
+
+const [nev2, kor2] = ['Odon', 50];
+
+console.log(nev2);
+console.log(kor2);
+
+
+const obj = {
+    keresztNev: 'Odon',
+    vezetekNev: 'Bodon'
+};
+
+const {keresztNev, vezetekNev} = obj;
+
+console.log(keresztNev);
+console.log(vezetekNev);
+
+const {keresztNev: x, vezetekNev: y} = obj;
+
+console.log(x);
+console.log(y);
+
+function korEsNyugdij(szuletesiEv){
+    let nyugdijKorhatar = 65;
+    const kor = new Date().getFullYear() - szuletesiEv;
+
+    return [kor, nyugdijKorhatar - kor];
+}
+
+const [kor3, nyugdij] = korEsNyugdij(1978);
+
+console.log(kor3);
+console.log(nyugdij);
+
+
+
+
+
+
+
+
+
+
+
