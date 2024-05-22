478a9203d1183228fc759c6c5bfda9103f74c78d
Roben Romeo <romeo.roben@katonaj-mkszig.hu>
2024-05-22 12:28:25 +0200

    asdasdasdasd

--- Roben Rómeó/js/js70/js/app.js
+++ Roben Rómeó/js/js70/js/app.js
@@ -43,7 +43,7 @@ function nyelvvizsga6(siker)
 nyelvvizsga6 (true);
 
 
-/*let i = 9;
+let i = 9;
 for ( let i =0 ; i < 5 ; i++)
 {
     console.log(i);
@@ -56,17 +56,17 @@ for ( var i =0 ; i < 5 ; i++)
 {
     console.log(i);
 }
-console.log(i);*/
+console.log(i);
 
 
 
-/*{
+{
     const a = 1;
     let b = 2;
     var c =3;
 
 }
-console.log(a+b*c);*/
+console.log(a+b*c);
 
 
 /*(function()
@@ -77,10 +77,10 @@ console.log(a+b*c);*/
 )();
 
 
-console.log(c);*/
+console.log(c);
 
 
-/*(function()
+(function()
 {
     let c =3;
     console.log(c);
@@ -88,21 +88,155 @@ console.log(c);*/
 )();
 
 
-console.log(c);*/
+console.log(c);
+const dobozok = document.querySelectorAll('.rectangle');
 
 
-
-
-
-let vezetekNev = 'teszt';
-let keresztNev = 'Elek';
-
-
-const szuletesiEv =1970;
-
-function korszamitas(ev)
+function szamok(a,b,c,d)
 {
-    return 2024 - ev;
+    return a+b+c+d;
+}
+ var osszeg = szamok (1,2,3,4);
+ console.log(osszeg);
+
+ var szamokTomb = [1,2,3,4];
+ var osszeg2 = szamok.apply(null, szamokTomb);
+
+ console.log (osszeg2);
+
+ var szamokTomb = [1,2,3,4];
+ const osszeg3 = szamok(...szamokTomb);
+ console.log(osszeg3);
+
+ const t2 = [5,6,7,8,9,10];
+
+ const t3 = [...szamokTomb,...t3];
+ console.log(t3);
+
+ const dobozok = document.querySelectorAll('.rectangle');
+ 
+ const cimsor = document.querySelector('h1');
+
+ const htmlElemek = [cimsor, ...dobozok]
+
+ Array.from(htmlElemek).forEach(aktualisElem => aktualisElem.style.color = 'purple');
+
+
+ function parosvagyparatlan()
+ {
+    var argumentumokTomb = Array.prototype.slice.call(arguments);
+    argumentumokTomb.forEach(function(aktualisElem)
+    {
+        if(aktualisElem % 2 === 0)
+        {
+            console.log('paros');
+
+        }
+        else
+        {
+            console.log('awawawawa');
+        }
+    });
+ }
+
+
+ parosvagyparatlan(1,2,3);
+
+
+ function parosvagyparatlan2(...szamok)
+ {
+    szamok.forEach(aktualisElem => 
+        {
+            if (aktualisElem % 2 === 0)
+            {
+                console.log('paros');
+            }
+            
+            else
+            {
+                console.log('paratlan');
+            }
+        })
+ }
+
+ parosvagyparatlan2(1,2,3);
+
+
+ function parosVagyParatlanEs6(teszt,...szamok)
+ {
+    szamok.forEach(aktualisElem => 
+        {
+            if(aktualisElem % 2 === 0)
+            {
+                console.log('paros ');
+            }
+            else
+            {
+                console.log('fasfasdf');
+            }
+        });
+ }
+
+parosVagyParatlanEs6('teszt', 1,2,3,10,20,25);
+
+function AdamsFamily(keresztNev, szuletesiEv,csaladNev)
+{
+    csaladNev === undefined ? csaladNev = 'Adams' : csaladNev=csaladNev;
+    
+    this.keresztNev = keresztNev;
+    this.szuletesiEv = szuletesiEv;
+    this.csaladNev = csaladNev;
+
+}
+var fester = new AdamsFamily('fester',1940);
+var mortisha = new AdamsFamily('Mortisha',1965);
+var kuzin = new AdamsFamily('Kuzin',1800,'Hogyishívják');
+
+
+function AdamsFamily(keresztNev, szuletesiEv,csaladNev = 'Adams')
+{
+   
+    
+    this.keresztNev = keresztNev;
+    this.szuletesiEv = szuletesiEv;
+    this.csaladNev = csaladNev;
+
+}
+var fester = new AdamsFamily('fester',1940);
+var mortisha = new AdamsFamily('Mortisha',1965);
+var kuzin = new AdamsFamily('Kuzin',1800,'Hogyishívják');*/
+
+const kerdes = new Map();
+kerdes.set('kerdes' , 'Hogy is hívják a de Miééért reklámbol a kisfiut');
+kerdes.set(1,'odon');
+kerdes.set(2,'ábel');
+kerdes.set(3,'Miklos');
+kerdes.set(4,'nándi');
+
+kerdes.set('helyes',2);
+
+kerdes.set(true,'helyes valasz');
+kerdes.set(false ,'nem talalt');
+
+console.log(kerdes.get('kerdes'));
+console.log(kerdes.size);
+
+if(kerdes.has(3))
+{
+    kerdes.delete(3);
 }
 
-console.log(vezetekNev+ '  ' + keresztNev+ ',szuletett' + szuletesiEv + '-ben.Most '+ korszamitas(szuletesiEv) +'éves.');
+kerdes.clear();
+
+kerdes.forEach
+(
+    (kulcs, ertek) => console.log(` kulcs: ${kulcs},ertek:${ertek}`)
+);
+
+for(let [kulcs, ertek] of kerdes.entries())
+{
+    if ( tyoeof(kulcs) === 'number')
+    {
+        console.log(`kulcs: ${kulcs},ertek:${ertek}`);
+    }
+}
