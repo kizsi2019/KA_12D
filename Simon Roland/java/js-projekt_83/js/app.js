6a57d68969c8560b6e82e394381fb8265ad197ad
Simon Roland <roland.simon@katonaj-mkszig.hu>
2024-05-30 13:42:59 +0200

    orai

--- Simon Roland/java/js-projekt_83/js/app.js
+++ Simon Roland/java/js-projekt_83/js/app.js
@@ -558,7 +558,7 @@ const elso = () => {
 };
 
 elso()
-*/
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 //callback hell
@@ -587,3 +587,52 @@ function receptLekerdez(){
 }
 
 receptLekerdez();
+*/
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//promise
+
+const azonositokLekerdezese = new Promise((resolve, reject) => {
+    setTimeout(() => {
+       resolve([676, 102, 34, 1489, 321]);
+
+    }, 2000);
+});
+
+const receptLekeres = (receptID) => {
+    return new Promise((resolve, reject) => {
+        setTimeout((id) => {
+            const recept = {
+                cim: 'gulyas leves',
+                kategoria:'levesek'
+            };
+            resolve(`${id}: ${recept.cim}`);
+        }, 2000, receptID);
+    });
+};
+
+const kategoriaLekeres = (kategoria) =>{
+    return new Promise((resolve, reject) =>{
+        setTimeout((kat) => {
+            const levesek = [
+                {cim:'tokleves', kategoria: 'levesek'},
+                {cim:'husleves', kategoria: 'levesek'}
+            ]
+        }, 2000, kategoria);
+    });
+};
+
+azonositokLekerdezese.then(azonositok => {
+    console.log(azonositok);return receptLekeres(azonositok[2]);
+})
+.then((recept)=>{
+    console.log(recept);
+    return kategoriaLekeres(recept.kategoria);
+})
+.then((kategoria)=>{
+    console.log(kategoria);
+})
+.catch(hiba => {
+    console.log(hiba);
+})
